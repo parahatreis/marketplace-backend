bebf6166075d66f82ef28e4c9a11e3955ce1626d
Parahat <parahat.iljanow@gmail.com>
2024-03-29 13:46:43 -0400

    refactoring

--- routes/v1/banners.js
+++ routes/v1/banners.js
@@ -15,41 +15,41 @@ const adminAuth = require("../../middleware/adminAuth");
 // @access Private (Admin)
 router.post('/', adminAuth, async (req, res) => {
 
-    const {
-        banner_name,
-        banner_url,
-    } = req.body;
+  const {
+    banner_name,
+    banner_url,
+  } = req.body;
 
-    if (!banner_name) return res.status(400).json({errors : 'Input Banner name'});
-    if (!banner_url) return res.status(400).json({errors : 'Input Banner URL'});
+  if (!banner_name) return res.status(400).json({ errors: 'Input Banner name' });
+  if (!banner_url) return res.status(400).json({ errors: 'Input Banner URL' });
 
-    try {
+  try {
 
-        const banner = await Banner.create({
-            banner_name,
-            banner_url
-        });
+    const banner = await Banner.create({
+      banner_name,
+      banner_url
+    });
 
-        res.json(banner);
+    res.json(banner);
 
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({errors : 'Server error'})
-    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ errors: 'Server error' })
+  }
 });
 
 // @route GET api/banners
 // @desc Get all Banners
 // @access Public
 router.get('/', async (req, res) => {
-    try {
-       const banners = await Banner.findAll();
-       res.json(banners);
-    }
-    catch (error) {
-       console.log(error);
-       res.status(500).send('Server error')
-    }
+  try {
+    const banners = await Banner.findAll();
+    res.json(banners);
+  }
+  catch (error) {
+    console.log(error);
+    res.status(500).send('Server error')
+  }
 });
 
 
@@ -57,21 +57,21 @@ router.get('/', async (req, res) => {
 // @desc Get Banner By id
 // @access Public
 router.get('/:banner_id', async (req, res) => {
-    try {
-        const banner = await Banner.findOne({
-            where : {
-                banner_id : req.params.banner_id
-            }
-        });
+  try {
+    const banner = await Banner.findOne({
+      where: {
+        banner_id: req.params.banner_id
+      }
+    });
 
-        if(!banner) return res.status(404).send('Banner Not found!')
+    if (!banner) return res.status(404).send('Banner Not found!')
 
-        res.json(banner);
-    }
-    catch (error) {
-        console.log(error);
-        res.status(500).json({errors : 'Server error'})
-    }
+    res.json(banner);
+  }
+  catch (error) {
+    console.log(error);
+    res.status(500).json({ errors: 'Server error' })
+  }
 });
 
 
@@ -80,63 +80,63 @@ router.get('/:banner_id', async (req, res) => {
 // @access Private (only Admin)
 router.patch('/:banner_id', adminAuth, async (req, res) => {
 
-    const newObj = {};
+  const newObj = {};
 
-    const {
-        banner_name,
-        banner_url,
-    } = req.body;
+  const {
+    banner_name,
+    banner_url,
+  } = req.body;
 
-    if (!banner_name) return res.status(400).send('Input Banner name');
-    if (!banner_url) return res.status(400).send('Input Banner URL');
+  if (!banner_name) return res.status(400).send('Input Banner name');
+  if (!banner_url) return res.status(400).send('Input Banner URL');
 
-    if (banner_name) newObj.banner_name = banner_name;
-    if (banner_url) newObj.banner_url = banner_url;
+  if (banner_name) newObj.banner_name = banner_name;
+  if (banner_url) newObj.banner_url = banner_url;
 
-    try {
+  try {
 
-        const banner = await Banner.findOne({
-            where : {
-                banner_id : req.params.banner_id
-            }
-        });
+    const banner = await Banner.findOne({
+      where: {
+        banner_id: req.params.banner_id
+      }
+    });
 
-        if(!banner) return res.status(404).send('Banner Not found!')
+    if (!banner) return res.status(404).send('Banner Not found!')
 
-        await Banner.update(newObj, {
-            where: {
-                banner_id: req.params.banner_id
-            }
-        });
+    await Banner.update(newObj, {
+      where: {
+        banner_id: req.params.banner_id
+      }
+    });
 
-        res.json("Updated!");
+    res.json("Updated!");
 
-    } catch (error) {
-        console.log(error);
-        res.status(500).send('Server error')
-    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Server error')
+  }
 });
 
 // @route DELETE api/banners/:banner_id
 // @desc Delete Store
 // @access Private (only Admin)
-router.delete('/:banner_id', adminAuth, async(req, res) => {
-    try {
-       const banner = await Banner.destroy({
-          where: {
-            banner_id: req.params.banner_id
-          }
-       });
- 
-       if (!banner) {
-          return res.status(404).send('banner not found');
-       }
-       res.json(banner);
+router.delete('/:banner_id', adminAuth, async (req, res) => {
+  try {
+    const banner = await Banner.destroy({
+      where: {
+        banner_id: req.params.banner_id
+      }
+    });
 
-    } catch (error) {
-       console.log(error);
-       res.status(500).send('Server error')
+    if (!banner) {
+      return res.status(404).send('banner not found');
     }
+    res.json(banner);
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Server error')
+  }
 });
 
 
@@ -145,59 +145,59 @@ router.delete('/:banner_id', adminAuth, async(req, res) => {
 // access Private(Admin)
 // Check file with multer
 const upload = multer({
-    limits: {
-       fileSize: 5000000
-    },
-    fileFilter(req, file, cb) {
-       if (!file.originalname.match(/\.(png|jpg|jpeg|svg|webp)$/)) {
-          return cb(new Error('Please upload image formats'))
-       }
-       cb(undefined, true)
+  limits: {
+    fileSize: 5000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(png|jpg|jpeg|svg|webp)$/)) {
+      return cb(new Error('Please upload image formats'))
     }
- });
- router.post('/image/:banner_id', adminAuth, upload.single('image'), async (req, res) => {
- 
-    try {
-        const banner = await Banner.findOne({
-            where : {
-                banner_id : req.params.banner_id
-            }
-        });
+    cb(undefined, true)
+  }
+});
+router.post('/image/:banner_id', adminAuth, upload.single('image'), async (req, res) => {
+  try {
+    const banner = await Banner.findOne({
+      where: {
+        banner_id: req.params.banner_id
+      }
+    });
 
-        if(!banner) return res.status(404).send('Banner Not found!')
+    if (!banner) return res.status(404).send('Banner Not found!')
+    if(!req.file) return res.status(500).send('Please upload valid image format!')
 
-        // Delete if categorie image already exists
-        if(banner.banner_image){
-            fs.unlinkSync(config.get('rootPath') + banner.banner_image);  
-        }
-
-        // Convert image to png with sharp
-        //    TODO Image Ratio
-        await sharp(req.file.buffer).resize({
-            width: 2400,
-            height: 800
-        }).webp().toFile(`./public/banner-images/${req.file.originalname+ '-' + banner.id}.webp`)
-
-        // Pathname of new banner image
-        let image = `/banner-images/${req.file.originalname + '-' + banner.id}.webp`;
-
-        banner.banner_image = image;
-
-        banner.save();
-
-        return res.send(image);   
+    // Delete if categorie image already exists
+    if (banner.banner_image) {
+      fs.unlinkSync(config.get('rootPath') + banner.banner_image);
     }
-    catch (error) {
-        console.log(error);
-        res.status(500).send('Server error')
-    }
- },
- (error, req, res, next) => {
+
+    // Convert image to png with sharp
+    //    TODO Image Ratio
+    await sharp(req.file.buffer).resize({
+      width: 2400,
+      height: 800
+    }).webp().toFile(`./public/banner-images/${req.file.originalname + '-' + banner.id}.webp`)
+
+    // Pathname of new banner image
+    let image = `/banner-images/${req.file.originalname + '-' + banner.id}.webp`;
+
+    banner.banner_image = image;
+
+    banner.save();
+
+    return res.send(image);
+  }
+  catch (error) {
+    console.log(error);
+    res.status(500).send('Server error')
+  }
+},
+  (error, req, res, next) => {
     res.status(400).send({
-       error: error.message
+      error: error.message
     })
- });
- 
+  });
+
 
 
 
